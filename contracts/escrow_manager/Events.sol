3a49b063fd617c01865890e74ea4928d6d324ddb
adambor <adamborteam@gmail.com>
2025-07-02 09:35:15 +0200

    Change signatures of some events

--- contracts/escrow_manager/Events.sol
+++ contracts/escrow_manager/Events.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.28;
 
 library Events {
-    event Initialize(address indexed offerer, address indexed claimer, bytes32 indexed escrowHash);
+    event Initialize(address indexed offerer, address indexed claimer, bytes32 indexed escrowHash, address claimHandler, address refundHandler);
     event Claim(address indexed offerer, address indexed claimer, bytes32 indexed escrowHash, address claimHandler, bytes witnessResult);
     event Refund(address indexed offerer, address indexed claimer, bytes32 indexed escrowHash, address refundHandler, bytes witnessResult);
     event ExecutionError(bytes32 indexed escrowHash, bytes error);
--- contracts/spv_swap_vault/Events.sol
+++ contracts/spv_swap_vault/Events.sol
@@ -7,6 +7,6 @@ library Events {
     event Opened(address indexed owner, uint96 indexed vaultId, bytes32 indexed btcTxHash, uint32 vout, SpvVaultParameters params);
     event Closed(address indexed owner, uint96 indexed vaultId, bytes32 indexed btcTxHash, bytes error);
     event Deposited(bytes32 indexed ownerAndVaultId, uint32 depositCount, uint64 amount0, uint64 amount1);
-    event Claimed(bytes32 indexed ownerAndVaultId, address indexed recipient, bytes32 indexed btcTxHash, bytes32 executionHash, address frontingAddress, uint32 withdrawCount, uint64 amount0, uint64 amount1);
-    event Fronted(bytes32 indexed ownerAndVaultId, address indexed recipient, bytes32 indexed btcTxHash, bytes32 executionHash, uint64 amount0, uint64 amount1);
+    event Claimed(bytes32 indexed ownerAndVaultId, address indexed recipient, bytes32 indexed btcTxHash, address caller, bytes32 executionHash, address frontingAddress, uint32 withdrawCount, uint64 amount0, uint64 amount1);
+    event Fronted(bytes32 indexed ownerAndVaultId, address indexed recipient, bytes32 indexed btcTxHash, address caller, bytes32 executionHash, uint64 amount0, uint64 amount1);
 }
