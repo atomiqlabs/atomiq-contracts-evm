67fa15a5154cbafc472ba653ecb4956a92374014
adambor <adamborteam@gmail.com>
2025-07-11 15:48:53 +0200

    Clarify the case when native token is used and claimer initiates

--- contracts/escrow_manager/EscrowManager.sol
+++ contracts/escrow_manager/EscrowManager.sol
@@ -40,6 +40,9 @@ contract EscrowManager is EscrowStorage, LpVault, ReputationTracker, EIP712Sigha
 
     //_extraData parameter is used for data-availability/propagation of escrow-specific extraneous data on-chain
     // and is therefore unused in the function itself
+    //Initiating the escrow with payIn=true as claimer with native token (0x0), will always fail, because
+    // we cannot use transferFrom for native token, therefore we require the offerer to initialize the
+    // escrow and deposit native tokens with msg.value
     function initialize(EscrowData calldata escrow, bytes calldata signature, uint256 timeout, bytes memory _extraData) external payable {
         //Check expiry
         require(block.timestamp < timeout, "init: Authorization expired");
@@ -198,6 +201,8 @@ contract EscrowManager is EscrowStorage, LpVault, ReputationTracker, EIP712Sigha
         }
     }
 
+    //For case where payIn=true and native token transfer is requested (token adddress 0x0)
+    // the src address needs to be the address of the transaction sender, since the msg.value is checked
     function _payIn(address src, address token, uint256 amount, bool payIn) internal {
         if(payIn) {
             TransferUtils.transferIn(token, src, amount);
