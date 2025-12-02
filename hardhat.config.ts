16ec0b311c80cba3c37f4c9eca6ab95f2162ed15
KSlashh <48985735+KSlashh@users.noreply.github.com>
2025-12-02 19:37:50 +0800

    restore hardhat.config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,9 +22,18 @@ const config: HardhatUserConfig & {networks: {[chainName: string]: {wethAddress:
   },
   etherscan: {
     apiKey: {
+      botanixMainnet: "botanix",
       goatTestnet: "goat",
     },
     customChains: [
+      {
+        network: "botanixMainnet",
+        chainId: 3637,
+        urls: {
+          apiURL: "https://api.routescan.io/v2/network/mainnet/evm/3637/etherscan",
+          browserURL: "https://botanixscan.io"
+        }
+      },
       {
         network: "goatTestnet",
         chainId: 48816,
@@ -39,6 +48,24 @@ const config: HardhatUserConfig & {networks: {[chainName: string]: {wethAddress:
     hardhat: {
       hardfork: "cancun"
     } as any,
+    citreaTestnet: {
+      url: "https://rpc.testnet.citrea.xyz",
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY],
+      wethAddress: "0x3100000000000000000000000000000000000006",
+      transferOutGasForward: 40_000
+    },
+    botanixTestnet: {
+      url: "https://node.botanixlabs.dev",
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY],
+      wethAddress: "0x0D2437F93Fed6EA64Ef01cCde385FB1263910C56",
+      transferOutGasForward: 40_000
+    },
+    botanixMainnet: {
+      url: "https://rpc.botanixlabs.com",
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY],
+      wethAddress: "0x0D2437F93Fed6EA64Ef01cCde385FB1263910C56",
+      transferOutGasForward: 40_000
+    },
     goatTestnet: {
       url: "https://rpc.testnet3.goat.network",
       accounts: [process.env.DEPLOYER_PRIVATE_KEY],
