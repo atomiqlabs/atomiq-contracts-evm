930f02cd0a7b87f1fd5c0387599493c2de129f8d
adambor <adamborteam@gmail.com>
2026-01-21 15:20:36 +0100

    Add more chains

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,10 +22,29 @@ const config: HardhatUserConfig & {networks: {[chainName: string]: {wethAddress:
   },
   etherscan: {
     apiKey: {
+      citreaTestnet: "citrea",
+      citreaMainnet: "citrea",
       botanixMainnet: "botanix",
       goatTestnet: "goat",
+      alpenTestnet: "alpen",
     },
     customChains: [
+      {
+        network: "citreaTestnet",
+        chainId: 5115,
+        urls: {
+          apiURL: "https://explorer.testnet.citrea.xyz/api",
+          browserURL: "https://explorer.testnet.citrea.xyz/"
+        }
+      },
+      {
+        network: "citreaMainnet",
+        chainId: 4114,
+        urls: {
+          apiURL: "https://explorer.mainnet.citrea.xyz/api",
+          browserURL: "https://explorer.mainnet.citrea.xyz/"
+        }
+      },
       {
         network: "botanixMainnet",
         chainId: 3637,
@@ -41,6 +60,14 @@ const config: HardhatUserConfig & {networks: {[chainName: string]: {wethAddress:
           apiURL: "https://explorer.testnet3.goat.network/api",
           browserURL: "https://explorer.testnet3.goat.network"
         }
+      },
+      {
+        network: "alpenTestnet",
+        chainId: 8150,
+        urls: {
+          apiURL: "https://explorer.testnet.alpenlabs.io/api",
+          browserURL: "https://explorer.testnet.alpenlabs.io/"
+        }
       }
     ]
   },
@@ -50,6 +77,12 @@ const config: HardhatUserConfig & {networks: {[chainName: string]: {wethAddress:
     } as any,
     citreaTestnet: {
       url: "https://rpc.testnet.citrea.xyz",
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY],
+      wethAddress: "0x3100000000000000000000000000000000000006",
+      transferOutGasForward: 40_000
+    },
+    citreaMainnet: {
+      url: "https://rpc.mainnet.citrea.xyz",
       accounts: [process.env.DEPLOYER_PRIVATE_KEY],
       wethAddress: "0x3100000000000000000000000000000000000006",
       transferOutGasForward: 40_000
@@ -70,6 +103,12 @@ const config: HardhatUserConfig & {networks: {[chainName: string]: {wethAddress:
       url: "https://rpc.testnet3.goat.network",
       accounts: [process.env.DEPLOYER_PRIVATE_KEY],
       wethAddress: "0xbC10000000000000000000000000000000000000",
+      transferOutGasForward: 40_000
+    },
+    alpenTestnet: {
+      url: "https://rpc.testnet.alpenlabs.io",
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY],
+      wethAddress: "0x790BE3d1FacC9c5499094a6f9F69aad2b9E04684",
       transferOutGasForward: 40_000
     }
   },
